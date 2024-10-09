a7ccae68f5f65ff8063f6ec1c6061c359d0176c4
shakiru102 <ashakiru53@gmail.com>
2024-10-09 20:04:49 +0100

    scroll

--- app/Landing/page.tsx
+++ app/Landing/page.tsx
@@ -2,7 +2,7 @@
 import Image from 'next/image'
 import React from 'react'
 import Logo from '@/app/assets/logo.png'
-import Link from 'next/link'
+// import Link from 'next/link'
 import Wine from '@/app/assets/wine.png'
 import Orange from '@/app/assets/orange.png'
 import Box from '@/app/assets/box.png'
@@ -11,7 +11,7 @@ import Face from '@/app/assets/face.png'
 import LInked from '@/app/assets/link.png'
 import Int from '@/app/assets/int.png'
 import Twit from '@/app/assets/twit.png'
-
+import { Link } from 'react-scroll';
 function Landing() {
     return (
         <main className='test w-full'>
@@ -19,14 +19,21 @@ function Landing() {
             <section className=' w-full flex justify-center items-center'>
                 <div className='w-[80%] flex justify-between ite py-4 items-center text-[16px] font-[400]'>
                     <div>
+                       
+                        <Link to=''>
                         <Image src={Logo} alt='logo' className='w-[124px] h-[27px]' />
+                        </Link>
                     </div>
                     <div className="">
                         <ul className="flex gap-6 items-center">
-                            <li><Link href="">About</Link></li>
-                            <li><Link href="">FAQ</Link></li>
+                            <li><Link to="about"  smooth={true}
+                    duration={500}
+                    className='cursor-pointer'>About</Link></li>
+                            <li><Link to="faq"  smooth={true}
+                    duration={500}
+                    className='cursor-pointer'>FAQ</Link></li>
                             <li className="bg-[#11D8FC] text-[#000000] p-2 rounded-[6px]">
-                                <Link href="">Join Waitlist</Link>
+                                <Link to="">Join Waitlist</Link>
                             </li>
                         </ul>
 
@@ -54,13 +61,22 @@ function Landing() {
                         <input type="email" placeholder="Your Email" className="email-input" />
                         <button className="join-button">Join Waitlist</button>
                     </div>
+                    <div className="flex items-center mt-4">
+                        <div className='flex'>
+                        <Image src={Face} alt="Image 1" className=" w-[40px] h-[40px]" />
+                        <Image src={Face} alt="Image 1" className="ml-[-22%] w-[40px] h-[40px]" />
+                        <Image src={Face} alt="Image 1" className="ml-[-22%] w-[40px] h-[40px]" />
+                        </div>
+                        <span>Join 5000+ Already Onboard</span>
+                    </div>
+
 
                 </div>
             </section>
 
             {/* about  */}
 
-            <section className='w-full flex flex-col items-center justify-center mt-20'>
+            <section className='w-full flex flex-col items-center justify-center mt-20' id='about'>
                 <div className='flex justify-between w-[50%] items-center'>
                     <h1 className='text-[60px] font-[400]'>Why <span className='text-[#0787A7]'>Haleo?</span></h1>
                     <p className='w-[360px] text-[16px]'>We help you achieve a balanced, vibrant lifestyle through consistent exercise and mindful living. <span className='text-[#0787A7]'>Haleo</span> supports you with:</p>
@@ -82,7 +98,7 @@ function Landing() {
             </section>
 
             {/* questions  */}
-            <section className='w-full flex flex-col items-center justify-center mt-40'>
+            <section className='w-full flex flex-col items-center justify-center mt-40' id='faq'>
                 <div className='w-[50%] flex justify-between'>
                     <div className='w-full  justify-between'>
                         <h1 className='text-[60px]'>Common <br /> Questions</h1>
@@ -104,18 +120,18 @@ function Landing() {
                             <p className='font-[600] text-[16px] mt-2'>Fitness & Wellness</p>
                         </div>
                         <div className='flex gap-[5px]'>
-                            <Image src={Face} alt='' className='w-[62.17px] h-[62.17px]'/>
-                            <Image src={Int} alt='' className='w-[62.17px] h-[62.17px]'/>
-                            <Image src={Twit} alt='' className='w-[62.17px] h-[62.17px]'/>
-                            <Image src={LInked} alt='' className='w-[62.17px] h-[62.17px]'/>
+                            <Image src={Face} alt='' className='w-[62.17px] h-[62.17px]' />
+                            <Image src={Int} alt='' className='w-[62.17px] h-[62.17px]' />
+                            <Image src={Twit} alt='' className='w-[62.17px] h-[62.17px]' />
+                            <Image src={LInked} alt='' className='w-[62.17px] h-[62.17px]' />
                         </div>
                     </div>
                     <div className='flex justify-between mt-10'>
                         <p>Copyright  @2024 Asteria. <br /> All Rights Reserved</p>
                         <div className="">
                             <ul className="flex gap-6 items-center text-[16px] font-[200]">
-                                <li><Link href="">Privacy Policy</Link></li>
-                                <li><Link href="">Terms Of Use</Link></li>
+                                <li><Link to="">Privacy Policy</Link></li>
+                                <li><Link to="">Terms Of Use</Link></li>
 
                             </ul>
 
